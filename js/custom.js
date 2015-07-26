bd0e9c186834d7e44040144a7e3d0ec51b3b4721
Tealtronics <tealtronics@gmail.com>
2015-07-26 11:11:34 -0500

    LA Mega Panama v0.1

---
+++ js/custom.js
@@ -0,0 +1,40 @@
+!function(d,s,id){var js,fjs=d.getElementsByTagName(s)[0],p=/^http:/.test(d.location)?'http':'https';if(!d.getElementById(id)){js=d.createElement(s);js.id=id;js.src=p+"://platform.twitter.com/widgets.js";fjs.parentNode.insertBefore(js,fjs);}}(document,"script","twitter-wjs");
+(function(d, s, id) {
+    var js, fjs = d.getElementsByTagName(s)[0];
+    if (d.getElementById(id)) return;
+    js = d.createElement(s); js.id = id;
+    js.src = "//connect.facebook.net/es_ES/sdk.js#xfbml=1&version=v2.4&appId=874327705975922";
+    fjs.parentNode.insertBefore(js, fjs);
+  }(document, 'script', 'facebook-jssdk'));
+      //<![CDATA[
+      $(document).ready(function(){
+        var stream = {
+          title: "98.3 F.M.",
+          mp3: "http://www.streaming507.com:9326/;stream.mp3"
+        },
+        ready = false;
+        $("#jquery_jplayer_1").jPlayer({
+          ready: function (event) {
+            ready = true;
+            $(this).jPlayer("setMedia", stream);
+          },
+          pause: function() {
+            $(this).jPlayer("clearMedia");
+          },
+          error: function(event) {
+            if(ready && event.jPlayer.error.type === $.jPlayer.error.URL_NOT_SET) {
+              // Setup the media stream again and play it.
+              $(this).jPlayer("setMedia", stream).jPlayer("play");
+            }
+          },
+          swfPath: "flash/jplayer",
+          supplied: "mp3",
+          preload: "none",
+          wmode: "window",
+          useStateClassSkin: true,
+          autoBlur: false,
+          keyEnabled: true
+        });
+
+      });
+    //]]>
